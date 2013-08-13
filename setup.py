b7185e1704dcc15fab7f772263b03a8672be4d73
antong liu <discomethod@qq.com>
2013-08-12 19:28:58 -0700

    no console on launch

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 # run from the command line using:
 # python setup.py py2exe
+#
+# python pyinstaller.py -F --noconsole D:/Coding/GitHub/pad-helper/pad-helper.py
 
+"""
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['pad-helper.py'])
+sys.argv.append('py2exe')
+
+setup(
+    options = {'py2exe': {'bundle_files': 1, 'compressed': True}},
+    windows = [{'script': "pad-helper.py"}],
+    zipfile = None,
+)
+"""
